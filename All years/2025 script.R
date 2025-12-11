cd49ef6ea8d8966f4151e2726a4e06d1f5defcc6
Michael Otis Clyne <michaelotisclyne@gmail.com>
2025-12-11 13:50:00 -0800

    Refactor and update barrel data processing scripts
    Major cleanup and refactoring of the R scripts for barrel experiment data. Removes legacy forecasting and plotting code, streamlines data wrangling, and adds new code for treatment assignment, germination calculations, and summary plots for ESA 2024 poster. Updates include improved use of dplyr/tidyr, new summary statistics, and integration of 2024 data. Also includes new and updated Excel and PDF data files for 2025.

--- All years/2025 script.R
+++ All years/2025 script.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(readxl)
 library(dplyr)
 library(conflicted)
-conflict_prefer("select", "dplyr")
+conflict_prefer("select", "dplyr", "filter")
 
 # Set WD for Desktop
 # setwd("~/GitHub/Barrels")
@@ -29,8 +29,8 @@ Cdata23 <- Cdata23 %>% # Remove all Unknown plants
 Cdata24 <- read_xlsx("2024 Data_Clean.xlsx", sheet = 1)
 Pdata24 <- read_xlsx("2024 Data_Clean.xlsx", sheet = 2)
 
-Cdata25 <- read_xlsx("Copy of 2025 Data.xlsx", sheet = 1) # counts of species in each barrel
-Pdata25 <- read_xlsx("Copy of 2025 Data.xlsx", sheet = 2)
+Cdata25 <- read_xlsx("2025 Data.xlsx", sheet = 1) # counts of species in each barrel
+Pdata25 <- read_xlsx("2025 Data.xlsx", sheet = 2)
 # clean it up
 Pdata25 <- Pdata25[, -3] # extra row
 
@@ -46,50 +46,57 @@ Cdata23 <- Cdata23 %>%
   rename_with(~ "total", .cols = any_of("count")) %>%  # rename count -> total if present
   mutate(Year = 2023) 
 
+Cdata23 <- Cdata23 %>%
+  pivot_wider(names_from = SPECIES,
+    values_from = total,
+    values_fill = 0) %>%      # non-seeded species counts = 0
+  relocate(BARREL, BRTE, LAGL, ELEL, .before = Year) %>% 
+  mutate( TrT = NA_character_)
+
+
+
+
 # 2024 #
 Cdata24 <- Cdata24 %>% 
   rename(., BARREL = `Barrel ID`, BRTE = `BRTE count`, LAGL = `LAGL count`, ELEL = `ELEL count`, ARTR = `ARTR count`) %>% 
   mutate(across(c(BRTE, LAGL, ELEL, ARTR), ~replace_na(., 0)))%>% 
-  mutate(BARREL = as.numeric(BARREL))
+  mutate(BARREL = as.numeric(BARREL), Year = 2024) 
 Cdata24 <- Cdata24[-161,]
 
 # 2025 #
 Cdata25 <- Cdata25 %>% 
   rename(., BARREL = Barrel, BRTE = `BRTE count`, LAGL = `LAGL count`, ELEL = `ELEL count`, ARTR = `ARTR count`) %>% 
   mutate(across(c(BRTE, LAGL, ELEL, ARTR), ~replace_na(., 0))) %>% 
-  mutate(BARREL = as.numeric(BARREL))
+  mutate(BARREL = as.numeric(BARREL), Year = 2025)
 Cdata25 <- Cdata25[-161,]
 
 view(Cdata25)
 
-# helper function to reshape and summarise a single year
-summarise_year <- function(df, year) {
-  df %>%
-    mutate(across(c(BRTE, LAGL, ELEL, ARTR), ~replace_na(., 0))) %>%
-    pivot_longer(
-      cols = c(BRTE, LAGL, ELEL, ARTR),
-      names_to = "SPECIES",
-      values_to = "count"
-    ) %>%
-    group_by(BARREL, SPECIES) %>%
-    summarise(total = sum(count), .groups = "drop") %>%
-    mutate(Year = year)
-}
 
 # apply to each dataset
-Cdata_all <- bind_rows(
-  Cdata23,
-  summarise_year(Cdata24, 2024),
-  summarise_year(Cdata25, 2025)
-) %>%
-  mutate(
-    BARREL = as.numeric(BARREL),
-    total  = as.numeric(total),
-    Year   = as.integer(Year)
-  ) %>%
-  arrange(Year, BARREL, SPECIES)
+Cdata_all <- bind_rows(Cdata23, Cdata24, Cdata25) %>%
+  mutate(BARREL = as.numeric(BARREL), Year = as.integer(Year)) %>%
+  arrange(BARREL, Year)
 
 head(Cdata_all, 20)
+
+# more tidy
+Cdata_all <- Cdata_all %>% 
+  relocate(., ARTR, .before = "Year") %>% # relocate ARTR
+  mutate(., across(c(BRTE, LAGL, ELEL, ARTR), ~replace_na(., 0))) %>%  # replace al 2023 no ARTR with 0 %>% 
+  select(., -TrT) 
+
+trt_lookup <- Cdata_all %>%
+  filter(!is.na(Trt)) %>% 
+  select(BARREL, Trt) %>%
+  distinct()          
+
+Cdata_all <- Cdata_all %>%
+  left_join(trt_lookup, by = "BARREL", suffix = c("", "_correct")) %>%
+  mutate(Trt = if_else(is.na(Trt), Trt_correct, Trt)) %>%
+  select(-Trt_correct)
+
+glimpse(Cdata_all)
 
 
 #############################
@@ -135,11 +142,12 @@ clean_pdata <- function(df, year) {
 # apply to each year
 Pdata23_clean <- clean_pdata(Pdata23, 2023)
 
-Pdata23_test <- Pdata23_clean %>% dplyr::select(., BARREL, SPECIES, PLANT_ID, TOOTHPICK, QUAD, DESIGNATION, HT_FINAL, FLWR_9, UID, Year) %>% 
+Pdata23_clean <- Pdata23_clean %>% dplyr::select(., BARREL, SPECIES, PLANT_ID, TOOTHPICK, QUAD, DESIGNATION, HT_FINAL, FLWR_9, UID, Year) %>% 
   mutate(., DESIGNATION = ifelse(SPECIES == "BRTE", "I-F",
                               ifelse(SPECIES == "LAGL", "N-F",
                                      ifelse(SPECIES == "ELEL", "N-M",
-                                            ifelse(SPECIES == "ARTR", "N-S", NA))))) %>% rename(.,FLWR_FINAL = FLWR_9)
+                                            ifelse(SPECIES == "ARTR", "N-S", NA))))) %>% 
+  rename(.,FLWR_FINAL = FLWR_9)
 
 
 Pdata24_clean <- clean_pdata(Pdata24, 2024)
@@ -161,6 +169,9 @@ Pdata_all <- Pdata_all %>% # Remove all Unknown plants
   subset(., SPECIES != "UD")
 
 
+glimpse(Pdata_all)
+
+
 
 
 
@@ -173,68 +184,101 @@ Pdata_all <- Pdata_all %>% # Remove all Unknown plants
 #   2b) species treatment on BRTE
 #   2c) repeat seeding on Slow LH species abundance over time
 
-# Assign each barrel the treatments and LH combos
-barrelkey <- read.csv("barrel_key.csv")
+# # Assign each barrel the treatments and LH combos
+# barrelkey <- read.csv("barrel_key.csv")
+# 
+# barrelkey$Species <- gsub("_[^_]+$","", barrelkey$Trt)
+# #remove trailing part about if control or not; irrelevant bc this is 1 year only
+# #barrelkey$LH_combo <- gsub("_[^_]+$","", barrelkey$LH_combo)
+# # remove the LH combo in the trt column
+# barrelkey$Trt <- gsub(".*_", "", barrelkey$Trt)
+# head(barrelkey)
+# #looks like it worked well; now to add life history key (manually)
+# 
+# unique(barrelkey$Trt)
+# barrelkey <- as_tibble(barrelkey %>% mutate(., Trt=as.factor(Trt)))
+# head(barrelkey)
+# #B = cheatgrass = F
+# #A = sagebrush = S
+# #L = whitedaisy = F
+# #E = bottlebrush = M
+# 
+# 
+# barrelkey <- barrelkey %>% 
+#   mutate(
+#     LH_combo = ifelse(Species == "LE" | Species == "BE", "FM", 
+#                       ifelse(Species == "BL", "FF",
+#                              ifelse(Species == "AL" | Species == "BA", "FS", NA)))
+#   )
+# 
+# head(barrelkey)
+# #now add treatment and LH combo info to the main dataframe
+# #make new columns:
+# Pdata_all$Trt = NA
+# Pdata_all$LH_combo = NA
+# Pdata_all$Spec.TRT=NA
+# #fill in based on info in barrel key:
+# for (i in 1:nrow(Pdata_all)) {
+#   Pdata_all$Trt[i] = barrelkey$Trt[barrelkey$BARREL == Pdata_all$BARREL[i]]
+#   Pdata_all$LH_combo[i] = barrelkey$LH_combo[barrelkey$BARREL == Pdata_all$BARREL[i]]
+#   Pdata_all$Spec.TRT[i] = barrelkey$Species[barrelkey$BARREL==Pdata_all$BARREL[i]]
+#   
+# }
+# 
+# # Cdata_all$Trt = NA
+# # Cdata_all$LH_combo = NA
+# # Cdata_all$Spec.TRT=NA
+# # #fill in based on info in barrel key:
+# # for (i in 1:nrow(Cdata_all)) {
+# #   Cdata_all$Trt[i] = barrelkey$Trt[barrelkey$BARREL == Cdata_all$BARREL[i]]
+# #   Cdata_all$LH_combo[i] = barrelkey$LH_combo[barrelkey$BARREL == Cdata_all$BARREL[i]]
+# #   Cdata_all$Spec.TRT[i] = barrelkey$Species[barrelkey$BARREL==Cdata_all$BARREL[i]]
+# #   
+# # }
 
-barrelkey$Species <- gsub("_[^_]+$","", barrelkey$Trt)
-#remove trailing part about if control or not; irrelevant bc this is 1 year only
-#barrelkey$LH_combo <- gsub("_[^_]+$","", barrelkey$LH_combo)
-# remove the LH combo in the trt column
-barrelkey$Trt <- gsub(".*_", "", barrelkey$Trt)
-head(barrelkey)
-#looks like it worked well; now to add life history key (manually)
-
-unique(barrelkey$Trt)
-barrelkey <- as_tibble(barrelkey %>% mutate(., Trt=as.factor(Trt)))
-head(barrelkey)
-#B = cheatgrass = F
-#A = sagebrush = S
-#L = whitedaisy = F
-#E = bottlebrush = M
-
-
-barrelkey <- barrelkey %>% 
-  mutate(
-    LH_combo = ifelse(Species == "LE" | Species == "BE", "FM", 
-                      ifelse(Species == "BL", "FF",
-                             ifelse(Species == "AL" | Species == "BA", "FS", NA)))
-  )
-
-head(barrelkey)
-#now add treatment and LH combo info to the main dataframe
-#make new columns:
-Pdata_all$Trt = NA
-Pdata_all$LH_combo = NA
-Pdata_all$Spec.TRT=NA
-#fill in based on info in barrel key:
-for (i in 1:nrow(Pdata_all)) {
-  Pdata_all$Trt[i] = barrelkey$Trt[barrelkey$BARREL == Pdata_all$BARREL[i]]
-  Pdata_all$LH_combo[i] = barrelkey$LH_combo[barrelkey$BARREL == Pdata_all$BARREL[i]]
-  Pdata_all$Spec.TRT[i] = barrelkey$Species[barrelkey$BARREL==Pdata_all$BARREL[i]]
-  
-}
-
-Cdata_all$Trt = NA
-Cdata_all$LH_combo = NA
-Cdata_all$Spec.TRT=NA
-#fill in based on info in barrel key:
-for (i in 1:nrow(Cdata_all)) {
-  Cdata_all$Trt[i] = barrelkey$Trt[barrelkey$BARREL == Cdata_all$BARREL[i]]
-  Cdata_all$LH_combo[i] = barrelkey$LH_combo[barrelkey$BARREL == Cdata_all$BARREL[i]]
-  Cdata_all$Spec.TRT[i] = barrelkey$Species[barrelkey$BARREL==Cdata_all$BARREL[i]]
-  
-}
+# count_new <- Cdata_all %>%
+#   group_by(BARREL, Trt, , Year) %>%
+#   summarise(total = sum(total, na.rm = TRUE), .groups = "drop") %>%
+#   spread(key = SPECIES, value = total, fill = 0)
 
 count_new <- Cdata_all %>%
-  group_by(BARREL, Trt, SPECIES, Year) %>%
-  summarise(total = sum(total, na.rm = TRUE), .groups = "drop") %>%
-  spread(key = SPECIES, value = total, fill = 0)
+  # Pivot species columns into long format
+  pivot_longer(
+    cols = c(BRTE, LAGL, ELEL, ARTR),
+    names_to = "Species",
+    values_to = "Count")
 
+print(range(count_new %>% filter(Species == "ARTR") %>% select(Count)))
 
-count_new <- count_new %>%
-  mutate(Trt = recode(Trt,
-                      `1` = "Control",
-                      `2` = "Repeated"))
+###################################
+#############
+
+### show counts of species by year
+ggplot(data = count_new, aes(x = factor(Year), y = Count, fill = Species)) +
+  geom_boxplot(outliers = F) +
+  geom_jitter(alpha = 0.3, width = 0.15)+
+  theme_bw() +
+  labs(x = "Year",
+    y = "Count") + 
+  facet_wrap(~Species, scales = "free_y") 
+
+ggplot(data = count_new, aes(x = factor(Year), y = Count, fill = Species)) +
+  geom_boxplot() +
+  theme_bw() +
+  labs(x = "Year",
+       y = "Count") + 
+  facet_wrap(~Species, scales = "free_y") 
+
+trts <- unique(as.factor(count_new$Trt))
+
+count_new_trt <- count_new %>%
+  mutate(Treatment = recode(Trt,
+                      LE_A = "Repeated" ,
+                      BE_A = "Repeated" ,
+                      BL_A= "Repeated" ,
+                      AL_A = "Repeated" ,
+                      BA_A = "Repeated" ,
+                      BL_1 = "Control" , AL_1 = "Control" , BE_1 = "Control" , BA_1 = "Control" , LE_1 = "Control" ))
 
 # 
 # count_new <- count_new %>%
@@ -244,23 +288,288 @@ count_new <- count_new %>%
 #          `N-S` = ARTR)
 
 
-count_long <- count_new %>%
-  pivot_longer(cols = c(BRTE, LAGL, ELEL, ARTR),
-               names_to = "SPECIES",
-               values_to = "Count")
+count_long <- count_new_trt 
 
 
-diffplot <- ggplot(count_long, aes(x = Trt, y = Count, fill = Trt)) +
+diffplot_25 <- ggplot(count_new_trt %>% filter(Year == 2025), 
+                   aes(x = Treatment, y = Count, fill = Treatment)) +
   geom_boxplot(alpha = 0.9) +
-  facet_wrap(~SPECIES, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
-  ggtitle("Plant Counts by Species and Treatment") +
+  facet_wrap(~Species, scales = "free_y") +
+  #theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+  ggtitle("Plant Counts by Species and Treatment", subtitle = "2025") +
   xlab("Treatment") + ylab("Plant Count")+
-  labs(fill = "Species") + 
+  labs(fill = "Treatment") + 
   theme_light()+
   scale_fill_brewer(palette = "Set2")
 
-diffplot
+diffplot_25
+
+
+diffplot_all <- ggplot(count_new_trt, 
+                       aes(x = factor(Year), y = Count, fill = Treatment)) +
+  geom_boxplot(outliers = F, alpha = 0.9) +
+  facet_wrap(~Species, scales = "free_y") +
+  #theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+  ggtitle("Plant Counts by Species and Treatment", subtitle = "All years") +
+  xlab("Treatment") + ylab("Plant Count")+
+  labs(fill = "Treatment") + 
+  theme_light()+
+  scale_fill_brewer(palette = "Set2")
+
+diffplot_all
+
+
+####### focus on one plot per species
+### LAGL
+ggplot(count_new_trt %>% filter(Species == "LAGL"), 
+       aes(x = factor(Year), y = Count, fill = Treatment)) +
+  geom_boxplot(outliers = F, alpha = 0.9) +
+#  facet_wrap(~Species, scales = "free_y") +
+  #theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+  ggtitle("LAGL abundance by Seeding Treatment", subtitle = "All years") +
+  xlab("Year") + ylab("Count")+
+  labs(fill = "Treatment") + 
+  theme_light()+
+  scale_fill_brewer(palette = "Set2")
+
+
+
+### ELEL
+ggplot(count_new_trt %>% filter(Species == "ELEL"), 
+       aes(x = factor(Year), y = Count, fill = Treatment)) +
+  geom_boxplot(outliers = F, alpha = 0.9) +
+  #  facet_wrap(~Species, scales = "free_y") +
+  #theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+  ggtitle("ELEL abundance by Seeding Treatment", subtitle = "All years") +
+  xlab("Year") + ylab("Count")+
+  labs(fill = "Treatment") + 
+  theme_light()+
+  scale_fill_brewer(palette = "Set2")
+
+
+### ARTR
+ggplot(count_new_trt %>% filter(Species == "ARTR"), 
+       aes(x = factor(Year), y = Count, fill = Treatment)) +
+  geom_boxplot( alpha = 0.9) +
+  #  facet_wrap(~Species, scales = "free_y") +
+  #theme(axis.text.x = element_text(angle = 30, hjust = 1)) +
+  ggtitle("ARTR abundance by Seeding Treatment", subtitle = "All years") +
+  xlab("Year") + ylab("Count")+
+  labs(fill = "Treatment") + 
+  theme_light()+
+  scale_fill_brewer(palette = "Set2")
+
+
+####### quick stats
+library(tidyverse)
+library(emmeans)
+library(multcomp)
+library(multcompView)
+
+df <- count_new  # or count_new_trt (either works)
+
+# ---- 1. Run ANOVA by species ----
+library(tidyverse)
+library(emmeans)
+library(multcomp)
+library(multcompView)
+
+anova_results <- count_new_trt %>%
+  group_by(Species) %>%
+  group_modify(~{
+    
+    df_sub <- .x   # .x is the data for each species
+    
+    # Run ANOVA
+    mod <- aov(Count ~ Treatment, data = df_sub)
+    
+    # Tukey
+    tk  <- emmeans(mod, pairwise ~ Treatment)
+    cld <- multcomp::cld(tk$emmeans)
+    
+    # Extract letters
+    cld_tbl <- cld %>%
+      as.data.frame() %>%
+      select(Treatment, .group)
+    
+    # Summary stats for annotation placement
+    sum_stats <- df_sub %>%
+      group_by(Treatment) %>%
+      summarise(
+        mean_count = mean(Count, na.rm = TRUE),
+        sd_count   = sd(Count, na.rm = TRUE),
+        .groups = "drop"
+      )
+    
+    # Return one row per treatment per species
+    left_join(sum_stats, cld_tbl, by = "Treatment") %>%
+      mutate(Species = unique(df_sub$Species))
+  }) %>%
+  ungroup()
+
+
+treatment_plot <- ggplot(count_new_trt, aes(x = Treatment, y = Count, fill = Treatment)) +
+  geom_boxplot(alpha = 0.9) +
+  facet_wrap(~Species, scales = "free_y") +
+  theme_light() +
+  ggtitle("Treatment Effects on Species Counts") +
+  ylab("Count") +
+  xlab("Treatment") +
+  scale_fill_brewer(palette = "Set2") +
+  
+  geom_text(
+    data = anova_results,
+    aes(
+      x = Treatment,
+      y = mean_count + sd_count * 2.2,   # scalable offset above box
+      label = .group
+    ),
+    size = 6,
+    fontface = "bold",
+    color = "black",
+    stroke = 0.35,       # adds white outline
+    linewidth = 0.35
+  )
+
+treatment_plot
+
+
+
+##################
+### Model time ###
+##################
+
+# simple linear model of seeds in to seeds out each year
+
+# start by setting up the seed data
+
+SDdata_23_calc <- Pdata23_clean %>% 
+  select(BARREL, SPECIES, FLWR_FINAL) %>% 
+  mutate(
+    Seed_tot = case_when(
+      SPECIES == "LAGL" ~ FLWR_FINAL * 39,
+      SPECIES == "ELEL" ~ FLWR_FINAL * 93,
+      SPECIES == "BRTE" ~ FLWR_FINAL * 88,
+      TRUE ~ 0
+    )
+  ) %>%
+  group_by(BARREL, SPECIES) %>% 
+  summarise(Total_seeds = sum(Seed_tot, na.rm = TRUE),
+            .groups = "drop")
+
+SDdata_23_test <- species_lookup_23 %>%
+  left_join(SDdata_23_calc, by = c("BARREL", "SPECIES")) %>%
+  mutate(Total_seeds = replace_na(Total_seeds, 0))
+
+
+
+SDdata_23 <- Pdata23_clean %>% 
+  select(BARREL, SPECIES, FLWR_FINAL, ) %>% 
+  group_by(BARREL, SPECIES) %>% 
+  mutate(
+    Seed_tot = case_when(
+      SPECIES == "LAGL" ~ FLWR_FINAL * 39,
+      SPECIES == "ELEL" ~ FLWR_FINAL * 93,
+      SPECIES == "BRTE" ~ FLWR_FINAL * 88,
+      TRUE ~ NA_real_)) %>% 
+  group_by(BARREL, SPECIES) %>% 
+  summarise(
+    Total_seeds = sum(Seed_tot, na.rm = TRUE),
+    .groups = "drop")
+
+SDdata_23_calc <- Pdata23_clean %>% 
+  select(BARREL, SPECIES, FLWR_FINAL) %>% 
+  mutate(
+    Seed_tot = case_when(
+      SPECIES == "LAGL" ~ FLWR_FINAL * 39,
+      SPECIES == "ELEL" ~ FLWR_FINAL * 93,
+      SPECIES == "BRTE" ~ FLWR_FINAL * 88,
+      TRUE ~ 0
+    )
+  ) %>%
+  group_by(BARREL, SPECIES) %>% 
+  summarise(Total_seeds = sum(Seed_tot, na.rm = TRUE),
+            .groups = "drop")
+
+
+
+
+
+
+
+SDdata_22 <- SDdata_23 %>%
+  mutate(Total_seeds = case_when(
+    SPECIES == "ELEL" ~ 105,
+    SPECIES == "LAGL" ~ 130,
+    SPECIES == "ARTR" ~ 230,
+    SPECIES == "BRTE" ~ 130,
+    TRUE ~ Total_seeds))
+
+SDdata_24 <- read_xlsx("2024 Data_Clean.xlsx", sheet = "seed count total")
+SDdata_24 <- SDdata_24 %>% select(`Barrel ID`, `BRTE Seeds`, `LAGL Seeds`, `ELEL Seeds`, `ARTR Seeds`) %>% 
+  rename(Barrel = matches("BARREL"),
+         BRTE = matches("BRTE"), LAGL = matches("LAGL"), ELEL = matches("ELEL"), ARTR = matches("ARTR"))
+SDdata_24 <- SDdata_24[-160,] # remove "total" row sum
+
+SDdata_24 <- SDdata_24 %>% 
+  pivot_longer(cols = c(BRTE, LAGL, ELEL, ARTR),values_to = "Total_seeds",
+               names_to =  "Species") 
+
+
+
+
+
+
+
+
+
+
+# model will have to wait
+################
+# next up is LAGL and repeat seeding on BRTE
+count_new_trt
+
+##### Plots counts of BRTE and LAGL by year and treatmnt
+ggplot(data = count_new_trt %>% filter(Species == c("BRTE", "LAGL")), ##### Plots counts of BRTE and LAGL by year and treatmnt
+       aes(x = Species, y = Count, fill = Treatment)) + 
+  geom_boxplot() + facet_wrap(~Year, scales = "free_y")
+
+count_wide <- count_new_trt %>%
+  select(BARREL, Year, Treatment,Trt, Species, Count) %>%
+  pivot_wider(
+    names_from = Species,
+    values_from = Count,
+    values_fill = 0)
+
+
+
+plot2 <- ggplot(count_wide,
+                aes(x = Treatment, y = BRTE, fill = Treatment)) +
+  geom_boxplot(alpha = 0.9) +
+  theme_bw() +
+  labs(
+    x = "",
+    y = "BRTE Count",
+    title = "Effect of Repeated LAGL Seeding on BRTE Counts"
+  ) +
+  guides(fill = "none")+ scale_fill_brewer(palette = "Set2")
+
+plot2
+
+
+int_plot <- ggplot(count_wide, 
+                aes(x = LAGL, y = BRTE, color = Treatment)) +
+  geom_point(alpha = 0.9) +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 1) +
+  theme_bw() +
+  labs(
+    x = "LAGL Count",
+    y = "BRTE Count",
+    title = "LAGL × Repeated Seeding on BRTE Abundance"
+  )+ scale_color_brewer(palette = "Set2")
+
+int_plot
+
 
 
 
@@ -301,7 +610,7 @@ count_long <- count_long %>%
 # 1) Figure of abundance changes year to year
 ggplot(count_long, aes(x = SPECIES, y = Count, fill = Trt)) +
   geom_boxplot(outliers = F) +
-  facet_wrap(~Year, scales = "free_y") + labs(
+  facet_wrap(~Year) + labs(
     title = "Abundance per barrel by Species Across Years",
     x = "Species",
     y = "Count",
@@ -313,6 +622,22 @@ ggplot(count_long, aes(x = SPECIES, y = Count, fill = Trt)) +
 count_long %>% filter(., SPECIES == "BRTE") %>% 
 ggplot( aes(x = Trt, y = Count, fill = Trt)) +
   geom_boxplot(outliers = F) 
+
+
+ggplot(count_long, aes(x = Year, y = Count, color = Species,
+                       group = interaction(BARREL, Species))) +
+  geom_line()
+
+########
+# line graph trend of counts of germinates by species per year
+species_means <- count_long %>%
+  group_by(Species, Year) %>%
+  summarise(mean_count = mean(Count, na.rm = TRUE),
+            .groups = "drop")
+ 
+ggplot(species_means, aes(x = Year, y = mean_count)) +
+  geom_line(aes(colour = Species), linewidth = 2)
+
 
 
 
